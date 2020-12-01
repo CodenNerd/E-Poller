174e64c8ba03c028b0076037dd39517d346a686b
CodenNerd <aatanda.dammy@gmail.com>
2020-12-01 11:06:17 +0100

    fix database name bug

--- build/index.js
+++ build/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,6 +28,8 @@ var v1_1 = __importDefault(require("./router/v1"));
 var connection_1 = __importDefault(require("./database/connection"));
 var exec_1 = __importDefault(require("./database/exec"));
 var config_1 = require("./database/config");
+var dotenv = __importStar(require("dotenv"));
+dotenv.config();
 var app = express_1.default();
 app.use(body_parser_1.json());
 app.use(body_parser_1.urlencoded({ extended: false }));
@@ -18,7 +39,7 @@ app.get('/', function (req, res) {
 });
 app.get('/createDB', function (req, res) {
     config_1.setDBExists(false);
-    var query = "CREATE DATABASE bincomphptest";
+    var query = "CREATE DATABASE " + process.env.DB_DB;
     connection_1.default.query(query, function (err, result) {
         res.send(err || result);
     });
