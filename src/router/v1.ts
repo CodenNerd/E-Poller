e9070a0c58669201da6211fc53facc512d17bd25
CodenNerd <aatanda.dammy@gmail.com>
2020-12-02 07:30:58 +0100

    correct summedLGAResults

--- src/router/v1.ts
+++ src/router/v1.ts
@@ -10,12 +10,12 @@ import summedLGAResults from './controller/summedLGAResults';
 const router = Router();
 router.use(json());
 
+router.get('/LGAs/:LGA/summedresults', summedLGAResults);
 router.get('/states', getStates)
 router.get('/states/:state/LGAs', getLGAs);
 router.get('/LGAs/:LGA/wards', getWards);
 router.get('/LGAs/:LGA/wards/:ward/pollingUnits', getPollingUnits)
 router.get('/pollingUnits/:pollingUnit/results', getPollingUnitResults);
-router.get('/LGAs/:LGA/summedresults', summedLGAResults);
 router.post('/pollingUnits/:pollingUnit/results', createPollingUnitResults)
 
 export default router;
